db2b6393ab4daab2d1b61a7652b3c70b62a70fe4
Juan Arias <juan.arias.freire@bbva.com>
2022-02-19 14:53:05 +0100

    Add Spring Boot test app + Hello Controller

---
+++ src/main/groovy/com/example/testingweb/TestingWebApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.testingweb
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class TestingWebApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(TestingWebApplication, args)
+	}
+
+}
